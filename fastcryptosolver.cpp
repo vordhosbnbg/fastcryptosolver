8573850aef6b2f70c8f38449012bab18f1ac2575
Boris Lazarov <vordhosbn.bg@gmail.com>
2016-08-19 19:54:06 +0300

    Optimized wordlist parsing

--- fastcryptosolver.cpp
+++ fastcryptosolver.cpp
@@ -7,44 +7,51 @@
 #include <iomanip>
 
 
-constexpr const char* wordlistName="../wordlist/english_big.txt";
+constexpr const char* wordlistName="../wordlist/english_huge.txt";
 
-std::unordered_set<std::string> LoadWordList(std::string filename)
+void LoadWordListIntoMap(const std::string filename, std::unordered_set<std::string>& outMap)
 {
-    std::unordered_set<std::string> retVal;
     std::cout << "Start loading wordlist from file: " << std::endl << filename << std::endl;
     auto tpBegin = std::chrono::system_clock::now();
     std::ifstream wordlistFile(filename);
+    outMap.reserve(2000000);
     if(wordlistFile.is_open())
     {
         std::string line;
+        // get length of file:
+        wordlistFile.seekg (0, wordlistFile.end);
+        int fileSize = wordlistFile.tellg();
+        wordlistFile.seekg (0, wordlistFile.beg);
+
         while(std::getline(wordlistFile, line))
         {
-            retVal.insert(line);
+            outMap.insert(std::move(line));
         }
         auto tpEnd = std::chrono::system_clock::now();
-
         auto millisecondsElapsed =  std::chrono::duration_cast<std::chrono::milliseconds>(tpEnd - tpBegin).count();
 
         double secondsElapsed = millisecondsElapsed / 1000.0;
 
         std::cout << "Wordlist loaded in " << std::setprecision(3) << std::fixed << secondsElapsed << " s" <<std::endl;
+        std::cout << "Word count: " << outMap.size() << " words" << std::endl;
+        std::cout << "Speed: " << std::setprecision(3) << fileSize / secondsElapsed / 1024 / 1024 << " MB/s" << std::endl;
+
     }
     else
     {
         std::cout << "Error opening file" << std::endl;
     }
-    return retVal;
 }
 
 
 int main()
 {
     std::string cryptogramText;
+    std::cout << "Enter cryptogram:" << std::endl;
     std::cin >> cryptogramText;
 
-    std::unordered_set<std::string> wordList = LoadWordList(wordlistName);
-    std::cout << "Loaded " << wordList.size() << " words" << std::endl;
+    std::unordered_set<std::string> wordList;
+    LoadWordListIntoMap(wordlistName, wordList);
 
 }
 
